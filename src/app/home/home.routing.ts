e799b8d1da5792d551438f13d6298c8a197dd8bf
Gleb <jongleb@yandex.ru>
2017-03-15 18:08:09 +0300

    to initial

--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -5,5 +5,5 @@ import {HomeComponent} from "./home.component";
 
 
 export const routing: ModuleWithProviders = RouterModule.forChild([
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ]);
